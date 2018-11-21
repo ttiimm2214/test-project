ea2d2b83efda3c6d9c3c230df4394572c52b22af
ttiimm2214 <timchen@umich.edu>
2018-11-21 17:27:02 +0800

    add

---
+++ link list.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct merge{
+    int val;
+    merge *next;
+};
+
+merge *head;
+  
+int add_number(int number){
+    merge *lastest;
+    lastest->val=number;
+    lastest->next=head;
+    head=lastest;
+}
+
+int print(merge *pr){
+    merge *temp;
+    temp=pr;
+    while(temp!=NULL){
+        cout<< temp->val;
+    }
+}
+
+int main()
+{
+    head=NULL;
+    add_number(1);
+    add_number(2);
+    add_number(4);
+    print (head);
+}
